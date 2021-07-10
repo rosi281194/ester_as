a680ec5919bc3d1cc72e21e59287c701b9b1c148
hyms <hdnymib@gmail.com>
2021-07-10 17:50:34 -0400

    base para la DB

---
+++ database/migrations/2021_07_10_000002_create_persona_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persona', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('ci')->unique();
+            $table->dateTime('fechaNacimiento');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId("escuadron")->constrained('escuadron');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persona');
+    }
+}
